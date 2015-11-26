257e70478e0bb49bcb6ba2f005d739273b194b06
evelynmarflo <pericotita10@gmail.com>
2015-11-26 15:46:54 -0500

    yg

--- app_conexion_jdbc_SanJose/src/app_conexion_jdbc_sanjose/App_conexion_jdbc_SanJose.java
+++ app_conexion_jdbc_SanJose/src/app_conexion_jdbc_sanjose/App_conexion_jdbc_SanJose.java
@@ -14,21 +14,12 @@ public class App_conexion_jdbc_SanJose {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
 // TODO code application logic here
 //      ESTE CODIGO ES PARA VER SI LA CONEXION ES SATIFACTORIA..
 //     -----  System.out.print("\n Ejercicio de conexion de BD\n");
 //        new pruebas.Pruebas_inicio().PruebaConexion();
 //        System.out.print("\n ------------------------------\n");
 // ------*---------------------*---------------------------*/*
-=======
-        // TODO code application logic here
-        //      ESTE CODIGO ES PARA VER SI LA CONEXION ES SATIFACTORIA..
-//          System.out.print("\n Ejercicio de conexion de BD\n");
-//        new pruebas.Pruebas_inicio().PruebaConexion();
-//        System.out.print("\n ------------------------------\n");
- //------*---------------------*--------------------------------*/*
->>>>>>> elizabeth/master
          presentacion.forms.JFrameMDI f = new presentacion.forms.JFrameMDI();
          f.setTitle("APLICACIÓN DE ACCESO A DATOS");
          f.setVisible(true);
